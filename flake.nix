18afde5fa56dcc95f1741d622514e7b80b7edfb8
Ethan Rooke <ethan@roo.ke>
2024-03-03 12:21:19 -0600

    remove avoidable sed call
    `jq` has a flag `-r` which prints raw output for consumption by other
    tool.

--- flake.nix
+++ flake.nix
@@ -20,8 +20,7 @@
         default = pkgs.writeShellScriptBin "update-input" ''
           input=$(                                           \
             nix flake metadata --json                        \
-            | ${pkgs.jq}/bin/jq ".locks.nodes.root.inputs | keys[]" \
-            | sed "s/\"//g"                                  \
+            | ${pkgs.jq}/bin/jq -r ".locks.nodes.root.inputs | keys[]" \
             | ${pkgs.fzf}/bin/fzf)
           nix flake lock --update-input $input
         '';
